8f04c1708d3d288267a9a7b9443c660f94d00c59
Andy <andypurbrick@gmail.com>
2019-02-13 15:52:04 +0000

    Latest commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 const drag1 = () => {
 return new PlainDraggable(document.getElementById('block1'))
 }
-
-drag1().snap = {x: {step: 10}, y: {step: 20}};
+// drag1().snap = {x: {step: 10}, y: {step: 20}};
 
 const drag2 = () => {
 return new PlainDraggable(document.getElementById('block2'))
 }
 
-drag2().snap = {x: {step: 10}, y: {step: 20}};
-
-const saveDivs = () => {
-
+const drag3 = () => {
+return new PlainDraggable(document.getElementById('block3'))
 }
 
+const drag4 = () => {
+return new PlainDraggable(document.getElementById('block4'))
+}
+
+const drag5 = () => {
+return new PlainDraggable(document.getElementById('block5'))
+}
+
+const drag6 = () => {
+return new PlainDraggable(document.getElementById('block6'))
+}
+
+const drag7 = () => {
+return new PlainDraggable(document.getElementById('block7'))
+}
+
+const drag8 = () => {
+return new PlainDraggable(document.getElementById('block8'))
+}
+
+const drag9 = () => {
+return new PlainDraggable(document.getElementById('block9'))
+}
+
+const drag10 = () => {
+return new PlainDraggable(document.getElementById('block10'))
+}
+
+const drag11 = () => {
+return new PlainDraggable(document.getElementById('block11'))
+}
+
+const drag12 = () => {
+return new PlainDraggable(document.getElementById('block12'))
+}
 
 const dropArea = document.querySelector('.dropArea');
+// dropArea.containment = {left: 20, top: 20, width: 600, height: 600};
 const fL = document.querySelector(".col-3");
 
 function furnitureListListener(){
@@ -23,7 +56,18 @@ function furnitureListListener(){
         if(e.target && e.target.matches(".placeholder-icon")){
             console.log(e.target)
             addToRoom(e.target)
-
+            drag1()
+            drag2()
+            drag3()
+            drag4()
+            drag5()
+            drag6()
+            drag7()
+            drag8()
+            drag9()
+            drag10()
+            drag11()
+            drag12()
         }});
 }
 
