87726db2a6630fa757bde3e394d59b3e06cf8256
Andy <andypurbrick@gmail.com>
2019-02-13 13:17:15 +0000

    Latest version

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-
-//
-// const a = displace(document.querySelector('#block1'));
-// const b = displace(document.querySelector('#block2'));
-// const c = displace(document.querySelector('#block3'));
-
 const drag1 = () => {
 return new PlainDraggable(document.getElementById('block1'))
 }
 
 drag1().snap = {x: {step: 10}, y: {step: 20}};
 
-//   // // containment: 'dropArea',
-//   // snap: draggable1.snap = {x: 130},
-//   // handle: '',
-//   // zIndex: 9000,
-//left: undefined,
-//top: undefined
-
 const drag2 = () => {
 return new PlainDraggable(document.getElementById('block2'))
 }
 
 drag2().snap = {x: {step: 10}, y: {step: 20}};
-//
-//
-// draggable2 = new PlainDraggable(document.getElementById('block3'));
+
+const saveDivs = () => {
+
+}
+
+
+const dropArea = document.querySelector('.dropArea');
+const fL = document.querySelector(".col-3");
+
+function furnitureListListener(){
+    fL.addEventListener('click', e =>{
+        if(e.target && e.target.matches(".placeholder-icon")){
+            console.log(e.target)
+            addToRoom(e.target)
+
+        }});
+}
+
+function addToRoom(furniture){
+  let furEl = new PlainDraggable(furniture)
+    dropArea.appendChild(furniture);
+  return furEl
+}
+furnitureListListener();
